eeae5be0349abd4e62225891674d24973cebe1e1
Vasiliy <kkolyasev1228@gmail.com>
2020-06-28 11:30:14 +0300

    Law

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,14 @@
 ;(function () {
 	
 	'use strict';
+	/**
+	 * подсвечивание последнего элемента (точки)
+	 * @type {jQuery}
+	 */
+	var str = $('#fh5co-logo a').text();
+	str = str.slice(0, -1) + '<span>'+ str.substr(-1) +'</span>';
+	$('#fh5co-logo a').html(str)
+
 
 	var isMobile = {
 		Android: function() {
--- header.php
+++ header.php
@@ -18,7 +18,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-xs-2">
-                        <div id="fh5co-logo"><a href="index.html">Law<span>.</span></a></div>
+                        <div id="fh5co-logo"><a href="<?=home_url('/');?>"><? bloginfo('name'); ?></a></div>
                     </div>
                     <div class="col-xs-10 text-right menu-1">
                         <? wp_nav_menu(array(
@@ -26,24 +26,6 @@
                             'container' => false,
                             'walker' => new Law_Header_Memu
                         )) ?>
-                       <!-- <ul>
-                            <li class="active"><a href="index.html">Home</a></li>
-                            <li><a href="practice.html">Practice Areas</a></li>
-                            <li><a href="won.html">Won Cases</a></li>
-                            <li class="has-dropdown">
-                                <a href="blog.html">Blog</a>
-                                <ul class="dropdown">
-                                    <li><a href="#">Web Design</a></li>
-                                    <li><a href="#">eCommerce</a></li>
-                                    <li><a href="#">Branding</a></li>
-                                    <li><a href="#">API</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="about.html">About</a></li>
-                            <li><a href="contact.html">Contact</a></li>
-                            <li class="btn-cta"><a href="#"><span>Login</span></a></li>
-                            <li class="btn-cta"><a href="#"><span>Sign Up</span></a></li>
-                        </ul>-->
                     </div>
                 </div>
 
