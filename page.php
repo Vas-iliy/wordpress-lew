2ef4509262a867786387358eff4a770fe9563716
Vasiliy <kkolyasev1228@gmail.com>
2020-06-30 18:48:16 +0300

    list

--- page.php
+++ page.php
@@ -14,25 +14,17 @@
 
 get_header();
 ?>
+<div class="container-fluid p-0">
+    <div class="row">
+        <div class="col col-md-12">
+           <?if(have_posts()): while (have_posts()): the_post();?>
+                <? the_content(); ?>
+            <?endwhile;?>
+            <?endif;?>
+        </div>
+    </div>
+</div>
 
-	<main id="primary" class="site-main">
-
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
 
 <?php
-get_sidebar();
-get_footer();
+get_footer();?>
